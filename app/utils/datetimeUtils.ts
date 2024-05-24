86cba19cef671eb355ad62373903ada3f200ca64
Ngọc Thắng (rotciv) <89723279+rotcivNT@users.noreply.github.com>
2024-05-24 09:20:40 +0700

    Update datetimeUtils.ts

--- app/utils/datetimeUtils.ts
+++ app/utils/datetimeUtils.ts
@@ -10,3 +10,16 @@ export const convertISOToDDMMYYY = (isoDate: string) => {
 
   return `${hours}:${minutes} ${day}/${month}/${year}`;
 };
+
+export const convertISOToDDMMYYYWithOutHour = (isoDate: string) => {
+  const date = new Date(isoDate);
+
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based in JavaScript
+  const year = date.getFullYear();
+
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  return `${day}/${month}/${year}`;
+};
